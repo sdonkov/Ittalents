8ac91cf49e7f9fc0e1e860d6976d417ccddcd3f1
sdonkov <sotirdonkov@gmail.com>
2022-06-18 21:34:59 +0300

    new

---
+++ Lesson3/Lesson3Task1.java
@@ -0,0 +1,16 @@
+public class Lesson3Task1 {
+
+    public static void main(String[] args) {
+
+        int a =3;
+        /*
+        00000000000000000000000000000011 --> 3
+        00000000000000000000000000000111 --> ?  3|7 3|5 BOTH WORKS
+        00000000000000000000000000000111 --> 7
+         */
+
+        System.out.println(3|7);
+        System.out.println(3|5);
+    }
+
+        }
