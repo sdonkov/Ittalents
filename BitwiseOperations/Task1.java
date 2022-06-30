240d6cab7f08eddf1d5d2e2be3be8321fb0ae267
sdonkov <sotirdonkov@gmail.com>
2022-06-30 15:55:06 +0300

    new

---
+++ BitwiseOperations/Task1.java
@@ -0,0 +1,15 @@
+public class Task1 {
+    public static void main(String[] args) {
+        /*
+            110 --> 6
+           &
+            100 --> 4
+            ---------
+            100
+
+         */
+
+        System.out.println(6&4);
+
+    }
+}
