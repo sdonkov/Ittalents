a7670342036cf260c4bce467fd92345f1f751715
sdonkov <sotirdonkov@gmail.com>
2022-06-30 15:54:47 +0300

    new

---
+++ Lesson1/src/Lesson2/TaskTryToGuessIncrement.java
@@ -0,0 +1,9 @@
+public class TaskTryToGuessIncrement {
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 0;
+        a= 15;
+        System.out.println(a++);
+        System.out.println(a--);
+    }
+}
